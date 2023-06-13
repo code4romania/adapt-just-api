ba7f4af0046661eeb651bc2e3ceb1552a0f84326
Catalin Iacob <catalin.iacob@web-group.ro>
2023-06-13 12:50:24 +0300

    Update 2023_06_11_100423_alter_complaints_table.php

--- database/migrations/2023_06_11_100423_alter_complaints_table.php
+++ database/migrations/2023_06_11_100423_alter_complaints_table.php
@@ -1,9 +1,7 @@
 <?php
 
-use Database\Seeders\LocationsDummyDataSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,10 +16,6 @@ return new class extends Migration
             $table->string('city_name')->nullable();
         });
 
-        Artisan::call('db:seed', [
-            '--class' => LocationsDummyDataSeeder::class,
-            '--force'     => true
-        ]);
     }
 
     /**
